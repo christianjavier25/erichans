a3f7444a79d95324c17b04981e8dbcb3a66d3156
Christian <25christianjavier@gmail.com>
2025-08-03 00:09:08 +0800

    Initial Commit

--- next.config.ts
+++ next.config.ts
@@ -2,11 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'export',
-  basePath: '/erichans',
-  assetPrefix: '/erichans/',
-
-
 };
 
 export default nextConfig;
